d95430f7aa18ec2bb1f0326020c348cc64d6ee68
seemamagar0 <seemaranamagar0@gmail.com>
2026-01-06 15:18:43 +0545

    urls change

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import happy_birthday
 
 urlpatterns = [
-    path("happy-birthday/", happy_birthday, name="happy_birthday"),
+    path("", happy_birthday, name="happy_birthday"),
 ]
